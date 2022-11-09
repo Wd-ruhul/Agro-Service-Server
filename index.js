337b65148c40db68145329e82d274779c20a94eb
Wd-ruhul <ruhul.wd@gmail.com>
2022-11-10 03:02:27 +0600

    create insert document

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-// require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w3d5mtp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("agro_service").collection("service");
+
+    //* create a document to insert
+
+    app.post("/add/service", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await serviceCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
