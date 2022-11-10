32696bdb77535ab491ca48939137bfdb8d9d8e7d
Wd-ruhul <ruhul.wd@gmail.com>
2022-11-10 17:44:44 +0600

    Create review insert api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,8 @@ async function run() {
   try {
     const serviceCollection = client.db("agro_service").collection("service");
 
+    const reviewCollection = client.db("agro_service").collection("reviews");
+
     //* create a document to insert
 
     app.post("/add/service", async (req, res) => {
@@ -34,7 +36,16 @@ async function run() {
       res.send(result);
     });
 
-    //* data read
+    //* create a document to insert review
+
+    app.post('/review', async (req, res) => {
+      const addReviews = req.body;
+      console.log(addReviews);
+      const result = await reviewCollection.insertOne(addReviews);
+      res.send(result);
+    });
+
+    //* data read with limit
 
     app.get("/homeservices", async (req, res) => {
       const query = {};
@@ -42,12 +53,20 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
-      app.get("/services", async (req, res) => {
-        const query = {};
-        const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-      });
+    //* read all Data
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    //* specific  data read
+    app.get("/servicedetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
   } finally {
     // await client.close();
   }
